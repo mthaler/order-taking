d4a969bd5e2c8721e440ca87b641f3b42038c273
Michael Thaler <michael.thaler@dlr.de>
2021-07-03 08:47:13 +0200

    Added createDecimal test.

--- src/test/kotlin/com/mthaler/ordertaking/validation/ConstrainedTypeTest.kt
+++ src/test/kotlin/com/mthaler/ordertaking/validation/ConstrainedTypeTest.kt
@@ -1,6 +1,7 @@
 package com.mthaler.ordertaking.validation
 
 import com.mthaler.ordertaking.OrderId
+import com.mthaler.ordertaking.Price
 import com.mthaler.ordertaking.Result
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
@@ -11,5 +12,11 @@ class ConstrainedTypeTest: StringSpec({
         createInt("test", ::OrderId, 0, 10, 5) shouldBe Result.Ok(OrderId(5))
         createInt("test", ::OrderId, 0, 10, -5) shouldBe Result.Error("test: Must not be less than 0")
         createInt("test", ::OrderId, 0, 10, 15) shouldBe Result.Error("test: Must not be greater than 10")
+    }
+
+    "createDecimal" {
+        createDecimal("test", ::Price, 2.0.toBigDecimal(), 4.0.toBigDecimal(), 3.0.toBigDecimal()) shouldBe Result.Ok(Price(3.0.toBigDecimal()))
+        createDecimal("test", ::Price, 2.0.toBigDecimal(), 4.0.toBigDecimal(), 1.0.toBigDecimal()) shouldBe Result.Error("test: Must not be less than 2.0")
+        createDecimal("test", ::Price, 2.0.toBigDecimal(), 4.0.toBigDecimal(), 5.0.toBigDecimal()) shouldBe Result.Error("test: Must not be greater than 4.0")
     }
 })
