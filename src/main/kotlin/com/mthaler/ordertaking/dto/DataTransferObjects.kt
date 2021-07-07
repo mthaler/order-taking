fd7c6a8546ec0b6f5f0285f6cd981223ab040ee0
Michael Thaler <michael.thaler@dlr.de>
2021-07-07 12:31:19 +0200

    Updated CustomerInfoDto.

--- src/main/kotlin/com/mthaler/ordertaking/dto/DataTransferObjects.kt
+++ src/main/kotlin/com/mthaler/ordertaking/dto/DataTransferObjects.kt
@@ -4,11 +4,8 @@ import com.mthaler.ordertaking.common.UnvalidatedCustomerInfo
 
 data class CustomerInfoDto(val firstName: String, val lastName: String, val emailAddress: String) {
 
-    companion object {
-        
-        /// Convert the DTO into a UnvalidatedCustomerInfo object.
-        /// This always succeeds because there is no validation.
-        /// Used when importing an OrderForm from the outside world into the domain.
-        fun toUnvalidatedCustomerInfo(dto: CustomerInfoDto): UnvalidatedCustomerInfo = UnvalidatedCustomerInfo(dto.firstName, dto.lastName, dto.emailAddress)
-    }
+    /// Convert the DTO into a UnvalidatedCustomerInfo object.
+    /// This always succeeds because there is no validation.
+    /// Used when importing an OrderForm from the outside world into the domain.
+    fun toUnvalidatedCustomerInfo(): UnvalidatedCustomerInfo = UnvalidatedCustomerInfo(firstName, lastName, emailAddress)
 }
