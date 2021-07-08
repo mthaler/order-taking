26e418bbf848c7134645b6f66923306c25741727
Michael Thaler <michael.thaler@dlr.de>
2021-07-08 08:33:47 +0200

    Added jackson dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 	implementation("io.arrow-kt:arrow-core:0.13.2")
+	implementation("com.fasterxml.jackson.core:jackson-core:2.12.3")
+	implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.3")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
 	testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
 	testImplementation("io.kotest:kotest-assertions-core:4.3.2")
 }
