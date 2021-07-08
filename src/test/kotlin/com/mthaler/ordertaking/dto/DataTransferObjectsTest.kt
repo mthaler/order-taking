3284ce6f68e4fd54ccd6ee702ee3ea1010ee3ffb
Michael Thaler <michael.thaler@dlr.de>
2021-07-08 16:15:13 +0200

    Added PlaceOrderEventDto.fromDomain test.

--- src/test/kotlin/com/mthaler/ordertaking/dto/DataTransferObjectsTest.kt
+++ src/test/kotlin/com/mthaler/ordertaking/dto/DataTransferObjectsTest.kt
@@ -26,7 +26,6 @@ class DataTransferObjectsTest: StringSpec({
                 "ZipCode: '1234' must match the pattern '\\d{5}'".invalidNel()
     }
 
-
     "OrderPlacedDto.fromDomain" {
         val event = PlaceOrderEvent.OrderPlaced(PricedOrder(OrderId("test"), CustomerInfo(PersonalName(String50("John"), String50("Doe")), EmailAddress("john.doe@example.com")),
             Address(String50("Wall Street"), None, None, None, String50("New York"), ZipCode("12345")), Address(String50("Wall Street"), None, None, None, String50("New York"), ZipCode("12345")),
@@ -35,5 +34,15 @@ class DataTransferObjectsTest: StringSpec({
         dto shouldBe OrderPlacedDto("test", CustomerInfoDto("John", "Doe", "john.doe@example.com"), AddressDto("Wall Street", "", "", "", "New York", "12345"),
             AddressDto("Wall Street", "", "", "", "New York", "12345"), 1000.0,
             listOf(PricedOrderLineDto("test", "W1234", 25, 40.0)))
+    }
+
+    "PlaceOrderEventDto.fromDomain" {
+        val event = PlaceOrderEvent.OrderPlaced(PricedOrder(OrderId("test"), CustomerInfo(PersonalName(String50("John"), String50("Doe")), EmailAddress("john.doe@example.com")),
+            Address(String50("Wall Street"), None, None, None, String50("New York"), ZipCode("12345")), Address(String50("Wall Street"), None, None, None, String50("New York"), ZipCode("12345")),
+            BillingAmount(1000.0), listOf(PricedOrderLine(OrderLineId("test"), ProductCode.WidgetCode("W1234"), OrderQuantity.UnitQuantity(25), Price(40.0)))))
+        val dto = PlaceOrderEventDto.fromDomain(event)
+        dto shouldBe PlaceOrderEventDto(mapOf("OrderPlaced" to OrderPlacedDto("test", CustomerInfoDto("John", "Doe", "john.doe@example.com"), AddressDto("Wall Street", "", "", "", "New York", "12345"),
+            AddressDto("Wall Street", "", "", "", "New York", "12345"), 1000.0,
+            listOf(PricedOrderLineDto("test", "W1234", 25, 40.0)))))
     }
 })
