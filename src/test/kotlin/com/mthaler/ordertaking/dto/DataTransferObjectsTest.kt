1c315a6bab34c75e44fbf8b0e46cf737b6a9407a
Michael Thaler <michael.thaler@dlr.de>
2021-07-08 10:50:26 +0200

    Added toAddress test.

--- src/test/kotlin/com/mthaler/ordertaking/dto/DataTransferObjectsTest.kt
+++ src/test/kotlin/com/mthaler/ordertaking/dto/DataTransferObjectsTest.kt
@@ -1,11 +1,9 @@
 package com.mthaler.ordertaking.dto
 
+import arrow.core.None
 import arrow.core.Valid
 import arrow.core.invalidNel
-import com.mthaler.ordertaking.domain.CustomerInfo
-import com.mthaler.ordertaking.domain.EmailAddress
-import com.mthaler.ordertaking.domain.PersonalName
-import com.mthaler.ordertaking.domain.String50
+import com.mthaler.ordertaking.domain.*
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -16,5 +14,12 @@ class DataTransferObjectsTest: StringSpec({
                 Valid(CustomerInfo(PersonalName(String50("John"), String50("Doe")), EmailAddress("john.doe@example.com")))
         CustomerInfoDto("John", "Doe", "john.doe").toCustomerInfo() shouldBe
                 "EmailAddress: 'john.doe' must match the pattern '.+@.+'".invalidNel()
+    }
+
+    "toAddress" {
+        AddressDto("Wall Street", "", "", "", "New York", "12345").toAddress() shouldBe
+                Valid(Address(String50("Wall Street"), None, None, None, String50("New York"), ZipCode("12345")))
+        AddressDto("Wall Street", "", "", "", "New York", "1234").toAddress() shouldBe
+                "ZipCode: '1234' must match the pattern '\\d{5}'".invalidNel()
     }
 })
